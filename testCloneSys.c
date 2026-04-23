8938fc7c468c1edc6d4674ac0cc96835b1b309c9
Joseph abi saad <jaa112@mail.aub.edu>
2026-04-23 23:22:02 +0300

    Update testCloneSys.c

--- testCloneSys.c
+++ testCloneSys.c
@@ -13,7 +13,7 @@ int main(void){
   char *stack = (char*)malloc(4096);
   stack = (char*)(((uint)stack + 4095) & ~0xfff);
   counter = 10;
-  int pid = clone(stack);
+  int pid = clone(thread_func, stack);
   if(pid < 0){
     printf(1, "clone failed\n");
     exit();
