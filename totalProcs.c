f8412c1f42442b6415b6fc19e7cd3dccb8141d8c
Joseph abi saad <jaa112@mail.aub.edu>
2026-04-23 13:18:24 +0300

    Update totalProcs.c

--- totalProcs.c
+++ totalProcs.c
@@ -2,18 +2,6 @@
 #include "user.h"
 
 int main(void){
-  int pid;
-  for(int i=0;i<3;i++){
-    pid = fork();
-    if(pid == 0){
-      for(int j=0;j<5;j++){
-        printf(1,"child %d is running, in loop %d\n" , getpid(), j);
-      }
-    exit();
-    }
-  }
-  for(int i=0;i<5;i++){
-    wait();
-  }
+  printf(1, "The number of processes is: %d\n", totalNumOfProcs());
   exit();
 }
