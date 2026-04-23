b8d82403ff39d2de216efcf1a1478de2920f3dfd
Joseph abi saad <jaa112@mail.aub.edu>
2026-04-23 22:56:03 +0300

    Update testCloneSys.c

--- testCloneSys.c
+++ testCloneSys.c
@@ -6,12 +6,16 @@ int count = 0;
 int main(void){
   count = 10;
   int pid = clone();
+  if(pid < 0){
+    printf(1, "clone failed\n");
+    exit();
+  }
   if(pid == 0){
     count++;
-    printf(1, "thread: counter = %d\n", count);
+    printf(1, "thread counter is %d\n", count);
     exit();
   }
   wait();
-  printf(1, "parent: counter = %d\n", count);
+  printf(1, "parent counter is %d\n", count);
   exit();
 }
