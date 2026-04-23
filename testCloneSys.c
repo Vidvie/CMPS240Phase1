de0120bd0fbbff58bc9fb913b5f4eecd60945c3a
Joseph abi saad <jaa112@mail.aub.edu>
2026-04-23 23:15:47 +0300

    Update testCloneSys.c

--- testCloneSys.c
+++ testCloneSys.c
@@ -10,15 +10,13 @@ void thread_func(void){
 }
 
 int main(void){
-  char *stack = malloc(4096);
+  char *stack = (char*)malloc(4096);
+  stack = (char*)(((uint)stack + 4095) & ~0xfff);
   counter = 10;
   int pid = clone(stack);
   if(pid < 0){
     printf(1, "clone failed\n");
     exit();
-  }
-  if(pid == 0){
-    thread_func();
   }
   wait();
   printf(1, "parent: counter = %d\n", counter);
